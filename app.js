733ccac7743c0520d42ba1b8e5087bc6380b3fb6
pratitb <pratit.bangdiwala7@gmail.com>
2024-05-25 10:53:47 +0530

    submit when rating selected

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 let ratingNumber = 0;
 
 $(document).ready(function () {
+    // ratings click
 	$('.ratings').on('click', function () {
 		$('.ratings').removeClass('activeRating');
 		$(this).addClass('activeRating');
+        $('.submit_btn').addClass('active_submit_btn');
         ratingNumber = this.innerText
         $('.rating_number').html(ratingNumber);
 	});
+
+    // submit click
     $('.submit_btn').on('click', function(){
         $('.feedback_wrap').removeClass('active_feedback_card');
         $('.thank_you_card').addClass('active_ty_card');
